866e2000d041a320b1c11359a1633d32a14a077e
rjoulfiand <rjoulfiand@gmail.com>
2026-08-26 08:57:42 +0700

    update js tipis tipis

--- js/script.js
+++ js/script.js
@@ -1,19 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    
-    // ==========================================
-    // 1. FITUR PENCARIAN (LIVE FILTERING)
-    // ==========================================
     const searchInput = document.getElementById('searchInput');
     const tableRows = document.querySelectorAll('tbody tr');
 
     searchInput.addEventListener('keyup', function(e) {
-        // Ambil teks yang diketik admin
         const term = e.target.value.toLowerCase();
-        
-        // Cek satu-satu setiap baris di tabel
         tableRows.forEach(row => {
             const rowData = row.textContent.toLowerCase();
-            // Kalau kata cocok, tampilkan. Kalau tidak, sembunyikan.
+            
             if(rowData.includes(term)) {
                 row.style.display = '';
             } else {
@@ -21,15 +14,11 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
-
-    // ==========================================
-    // 2. SISTEM ALERT STOK HABIS (OTOMATIS)
-    // ==========================================
     function checkLowStock() {
         let barangHabis = [];
 
         tableRows.forEach(row => {
-            // Ambil angka stok dari elemen yang punya class .badge
+            
             const stokElement = row.querySelector('.stok-angka .badge');
             
             if (stokElement) {
@@ -39,19 +28,15 @@ document.addEventListener('DOMContentLoaded', function() {
                     const namaBarang = row.cells[1].textContent;
                     barangHabis.push(namaBarang);
                     
-                    // Kasih efek visual warna merah muda ke baris tabelnya
                     row.classList.add('row-danger');
                 }
             }
         });
         if (barangHabis.length > 0) {
-            // Delay sedikit biar halaman ke-load dulu baru alert muncul
             setTimeout(() => {
                 alert("⚠️ PERINGATAN SISTEM: KEKURANGAN STOK!\n\nBarang berikut telah habis stoknya:\n- " + barangHabis.join("\n- ") + "\n\nMohon segera hubungi Vendor untuk restock!");
             }, 500);
         }
     }
-
-    // Jalankan sistem pengecekan stok otomatis saat web dibuka
     checkLowStock();
 });
